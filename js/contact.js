4e40e7ab50ccab83c4afaf808c291baf5f50cd39
GuoFM <fguo1996@gmail.com>
2025-02-10 08:46:00 +0800

    Sync latest changes

--- js/contact.js
+++ js/contact.js
@@ -23,13 +23,8 @@ function sendEmail(event) {
                 
                 // 使用 setTimeout 确保提示显示后再跳转
                 setTimeout(() => {
-                    // 获取当前 URL 并构建跳转路径
-                    const currentUrl = window.location.href;
-                    const thankyouUrl = currentUrl.substring(0, currentUrl.lastIndexOf('/contact')) + '/thank-you.html';
-                    console.log('Redirecting to:', thankyouUrl);
-                    
-                    // 使用 window.location.assign 进行跳转
-                    window.location.assign(thankyouUrl);
+                    // 直接跳转到同一目录下的 thank-you.html
+                    window.location.href = './thank-you.html';
                 }, 500);
             })
             .catch(function(error) {
